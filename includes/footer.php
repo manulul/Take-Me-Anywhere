4d766b126a2dd8c6c708e4ae2c85d2101a768873
mathias <mathiasqvist@hotmail.com>
2017-02-08 10:41:22 +0100

    her er index fil og includes

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+	<h3>Designed and powered by MEKJ - dev &#169; All rights reserved</h3>
+</footer>
+
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/master.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Take Me Anywhere</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Lato" rel="stylesheet"> </head>
+
+<body>
