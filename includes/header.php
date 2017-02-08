b11543b96577a836f7fdec860f77a547a43ee3e7
mathias <mathiasqvist@hotmail.com>
2017-02-08 13:52:03 +0100

    jeg har lavet forsiden med globus og knappe

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <title>Take Me Anywhere</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Lato" rel="stylesheet"> </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php 
 	include 'includes/header.php';
 ?>
-
-
-
-
-
-
+    <section id="worldmap"> <img src="images/worldmap.png">
+        <div class="wrapper">
+            <button type="button" id="knap">Take Me Anywhere</button>
+        </div>
+    </section>
     <?php 
 	include 'includes/footer.php';
 ?>
