8aebe34eff828cc8ad8ad58067d938ceb1a06394
mathias <mathiasqvist@hotmail.com>
2017-02-13 15:29:56 +0100

    har rettet vores js filer og tilføjet jquery

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
 <footer>
-	<h3>Designed and powered by MEKJ - dev &#169; All rights reserved</h3>
+    <<<<<<< HEAD <h3>Designed and powered by MEKJ - dev &#169; All rights reserved</h3>
 </footer>
-
-<script src="js/jquery-3.1.1.min.js"></script>
-<script src="js/master.js"></script>
+<script src="js/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<h3>Designed and powered by MEKJ - dev &#169; All rights reserved</h3> </footer>
 </body>
+
 </html>
