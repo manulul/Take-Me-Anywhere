03c5c332d8901d0689f1ed9b378b63e9cccc9d80
HughMungusHorse <kavi1989@gmail.com>
2017-02-14 12:34:14 +0100

    fikset mathias jquery rotation

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,6 @@
 <footer>
     <h3>Designed and powered by MEKJ - dev &#169; All rights reserved</h3> </footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://unpkg.com/vue/dist/vue.js"></script>
-<script src="js/jquery.js"></script>
-<script src="js/master.js"></script>
+
 </body>
 
 </html>
