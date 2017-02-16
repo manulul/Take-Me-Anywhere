b1acd37a9201babe4714628e7ef4fdbb7733c884
mathias <mathiasqvist@hotmail.com>
2017-02-16 13:37:11 +0100

    PHP
    Fikset array, samt nu kalder den 1 input, mangler at tilpasse det til
    image.

--- includes/array.php
+++ includes/array.php
@@ -1,18 +1,12 @@
 <?php
-$a=array("Datça", "Shikaoi", "Santiago de Cuba", "Calgary", "Tromsø", "Limerick", "Nuuk", "Chihuahua", "Limón", "Val Paraíso", "Cape Town", "Gibraltar", "Algiers", "Kuala Lumpur", "Fluk", "Darwin", "Isle of Man", "Rønne", "St. Petersborg", "Pyongnyang", "Sichuan", "Xigazê", "Goa", "Riyadh", "Dar es Salaam", "Bata", "Mu Ko Similan", "Haifa", "Chania", "Prag", "Marseille", "Amsterdam", "Keszthely", "Firenze", "Bonifacio", "Bissau", "Curacao", "Ishigaki", "Anchorage", "Lebanon", "Albuquerque", "Hilo", "Antananarivo", "Kathmandu", "Göteborg", "Christchurch", "Samoa", "Munich", "Wien", "Helsinki");
-$random_keys=array_rand($a,2);
-echo $a[$random_keys[0]]."!";
+$a=array("Datca", "Shikaoi", "Santiago de Cuba", "Calgary", "Tromsoe", "Limerick", "Nuuk", "Chihuahua", "Limon", "Val Paraiso", "Cape Town", "Gibraltar", "Algiers", "Kuala Lumpur", "Fluk", "Darwin", "Isle of Man", "Roenne", "Saint Petersburg", "Pyongnyang", "Sichuan", "Xigaze", "Goa", "Riyadh", "Dar es Salaam", "Bata", "Mu Ko Similan", "Haifa", "Chania", "Prague", "Marseille", "Amsterdam", "Keszthely", "Florence", "Bonifacio", "Bissau", "Curacao", "Ishigaki", "Anchorage", "Lebanon", "Albuquerque", "Hilo", "Antananarivo", "Kathmandu", "Gothenburg", "Christchurch", "Samoa", "Munich", "Vienna", "Helsinki");
+$random_key=array_rand($a,1);
+
+echo $a[$random_key]."!";
+
+$a = str_replace(' ', '_', $a);
+
+echo $a[$random_key].".jpg";
+
+
 ?>
- 
-
-
-<?php
-if ($a == 0) {
-    echo "Turkey";
-} elseif ($a == 1) {
-    echo "China";
-} else {
-    echo "No mans land";
-}
-?>
-
--- index.php
+++ index.php
@@ -1,36 +1,26 @@
 <?php 
 	include 'includes/header.php';
 ?>
-
-
-
     <section id="worldmap"> <img src="images/worldmap.png">
         <div class="wrapper">
-            <button type="button" onclick="playSound();" id="knap" >{{ message }}</button>
+            <button type="button" onclick="playSound();" id="knap">{{ message }}</button>
         </div>
     </section>
-   
-
     <script>
- var wrapper = new Vue({
+        var wrapper = new Vue({
             el: '.wrapper'
             , data: {
                 message: 'Take Me Anywhere'
             }
         })
     </script>
-   
-    
-<article class="modal">
-<img> </img>
-<h1 id="result">You are going to 
+    <article class="modal"> <img src="images/<?php include 'includes/array.php';
+?>" alt="Pictures of destination" </img>
+        <h1 id="result">You are going to 
 <?php
 include 'includes/array.php';
 ?>
-</h1>
-</article>
-
-<?php 	
+</h1> </article>
+    <?php 	
 	include 'includes/footer.php';
 ?>
-
