ba1279bb5c557bec2a1f57b172906e9dc9112e41
mathias <mathiasqvist@hotmail.com>
2017-02-17 12:26:37 +0100

    Lavet php og styling
    Lavet php info boks med switch metoden, og derefter stylet p’tagget

--- index.php
+++ index.php
@@ -21,7 +21,12 @@
 <?php
 echo $a[$random_key]."!";
 ?>
-</h1> </div> <img id="resultimg" src="images/<?php $a = str_replace(' ', '_', $a); echo $a[$random_key]
+
+</h1>
+            <p id="tekstboks">
+                <?php include 'includes/info.php';?>
+            </p>
+        </div> <img id="resultimg" src="images/<?php $a = str_replace(' ', '_', $a); echo $a[$random_key]
 ?>.jpg" alt="Pictures of destination" </img>
         <a href="index.php">
             <button type="button" id="knapto">Try again!</button>
